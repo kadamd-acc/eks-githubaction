1e7e40fc031650a85847cd21352609a96d664c83
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-06-02 18:33:15 +0530

    testing2

--- Provider.tf
+++ Provider.tf
@@ -2,9 +2,9 @@
 
 provider "aws" {
   region     = var.region_name
-  access_key = "ASIA2ZCHYB753DHA3PH6"
-  secret_key = "lHC0Z7+J0ocoPxCY9cGPsvvDQCBBhCEyF6KsNAhe"
-  token      = "IQoJb3JpZ2luX2VjEGUaCXVzLWVhc3QtMSJHMEUCIQDvFYpjvx7sbCoN05uguIgtN3Trqky2PkRAB3//HeM0vQIgcYNhGZ6PbeGsJaVdkRQ5q9gfRQrUk1bd+8aEuXgJWl4qrwIIXRACGgw3NDEwMzIzMzMzMDciDHtz0LnAPtRPpPtcBSqMAjYnvqe0COgG1fyIhs2jqebIOYYU/GoGgsRInZJRS3SBmx0hZHtKS5l/7SUttvXp1gyRNrjholdrvlniuk0igizvuLCvvsX7BaXcSJZ5lr+lALSMVf625emCTnzk2s7bljqoJsAR+ksqCKfOI3bEO5N9dCwOtpH+B8HWs9TzgavA8KS5KSCHhkRwSwAqqveqimIqIdnDjPOtIp6WfbanhzFsXqzZAgvsT6mDJEegK2s+7EKm13HHZ5lilMo1Ulr/E0WpEuNkTbE24oIXAIsM7b7oVIRBnq5UNlBiDHQ06+spM/TaoZeWonD89LnlMwdtrxtJMsN8yur08IXkD8wpob1B8f4dslFn1FfhGmgwpdHilAY6nQGOzh/mvp1mgiu2kyz6pp609DmO67hWq7xZ1KSOLPX61qyYY2v+vuXNkdr4MxVaTdbmsjuvSRZuFB9pSBVOcIL8xDHWYpicYcwMNv8Cb+iu4TYpjTUXrS1niqjAtdoF5i5tT5R2Sf0ws5VeW8Zo4voY41OJodL/KeZu5y4pHYwMQE6RCuZYoj1kG3x4zPSKkjKfFSDeNHK/pmq0noNn"
+  access_key = "ASIA2ZCHYB756NYSLMUR"
+  secret_key = "hsQJ/RR7UpybVMDO4FVSD+lCjAdVH7f3Fk0ZFfeS"
+  token      = "IQoJb3JpZ2luX2VjEGUaCXVzLWVhc3QtMSJIMEYCIQDnmtuTyxP2njK7PFPtR+YV8WIgDeOKLtWDjbVvxzsUzgIhAKNx+RoL5I1oqqo5cLOM7HWFvLe3/ocvUPGufAy6cHkaKq8CCF4QAhoMNzQxMDMyMzMzMzA3IgzKz/bfR4MUkhthCKoqjAK6ZLSYJvv0P6Am6hFznlTweWweIhHRJIPadSTMiU20gUY+Y3NG75w/UCaAvrcj1zw03WDJ+L0mahBDvhnhvh9sMUKPkXq8o7ZtwRl1RYVjvRA4VWekH8wXMi2WEhGS1AbR4KG6cqONxA1i4aKBuK2fENptJwTAN5s6QffEhtUyEHta5rdJ2le/7L5qY+waLB40hpY5EizhyHMmzcZvyuJidSEqHqGaiPGguTPHgbo9RIeqq1sUvac1q6CFQJr1bO7Qukf7C0TzKq5KoWHVJ2wmZrLSoR1ccTSYKsBzKvns8f7cfxf/meNz1ltHAlQgN1yozvnTto/q9sFlurOsU2X2wnc9sNwzwxPn3jLOMKnp4pQGOpwBZ+5AyWASGr45SqKk7tCzZX7zOAB/FaHOZ1N1jKBEhNtrEmdKBta8J05Y5Kx9/rOBVXS8tAGdh43el2s4cablUElSJANBzJsHrhce73JeudEDz9/bStPkd4lyYr6g/Zqk9jwc546LQOzyNfSDOEd61JIBvbDeUcfIUyOvq04XbNdueBCmAwhbzkNEiSC3JswdEVwNIwIxfqBc7CON"
 }
 
 terraform {
@@ -17,23 +17,23 @@ terraform {
   }
 }
 #
-#resource "aws_s3_bucket" "terraform-state" {
-#  bucket = "landg-terraform-state"
-#  lifecycle {
-#    prevent_destroy = true
-#  }
-#  versioning {
-#    enabled = true
-#  }
-#  server_side_encryption_configuration {
-#    rule {
-#      apply_server_side_encryption_by_default {
-#        sse_algorithm = "AES256"
-#      }
-#    }
-#  }
-#
-#}
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "landg-terraform-state"
+  lifecycle {
+    prevent_destroy = true
+  }
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+}
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
