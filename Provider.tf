c152edd6daa973dd756fe644157d60f203d5b396
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-06-06 14:50:48 +0530

    updated the git action

--- Provider.tf
+++ Provider.tf
@@ -5,9 +5,6 @@
 
 provider "aws" {
   region     = var.region_name
-  access_key = "ASIA2ZCHYB75SZIKQY55"
-  secret_key = "kPCgo8sxmg5koOom2NhsbqOssDeGDpNV+S60URIH"
-  token      = "IQoJb3JpZ2luX2VjEMD//////////wEaCXVzLWVhc3QtMSJHMEUCIQDHMV/2uHqem54wb+ALfdnAbfgB2YgKO7gDSPWuYp+2rwIgCCDJ+PfFlYDWIuY+AHoT/gLd8y7M44D1AdbOUSEpf2oquAIIuf//////////ARACGgw3NDEwMzIzMzMzMDciDOzJgUawaxp7OMAsHSqMAqfraSBkX1GkLUFkdBrtNQ+BHZjpScJ4ZBVapLwKU7yooTC/vUEfDBS0AGnQXcXHXEbnwXyPIKhD5/ucxF7odjebN42h74vChOoIV5APBd68Cq826+41Lx/xJ1V2X7t6Zz8rhsSfhu8g8vY2+vMG8RKVKs+jWCwJOI/GBF7PpnrdmezkFfcQhV2QLw4f6popdmsfVfgjpam3tQgwO57bBQxNXJaeOCm190G05KPe+ecRIcJzjlyVALZDAi/zmLgXaQlGpeQ4KA7kIGy0USmp5tOdWXTmOBt7azqpa60cxRXS7oIXyI/OXok+oQYlmNSfDgY+c4Qzuy7kKXuuXeU0BaHb1zztW2ntsFXqFl8wkdn2lAY6nQHPrYcsRkgZSNJMoRxkv3dK2RUWrpnJ8PehgKgcQNv74qsGTqtwbvnudC8vZVNwN9Sdyag1oz9u07wURy21V21hg3hp5ixLdDNHV93hFGK5CPXfjRZDKz4tv9gj4+7hFa5V+Lv+sCZA3HTFBfce5/aYltrRt7HMF76sz+IZ5y4W6WFBHRWv3gHeZvIFZUuKEmjinOm1bSHc5V+iUQDP"
 }
 terraform {
   backend "s3" {
