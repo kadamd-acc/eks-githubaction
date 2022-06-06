a85590875ab6fbf91052210bad7b3b1d91ab0862
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-06-06 20:42:21 +0530

    reverted the changes

--- Provider.tf
+++ Provider.tf
@@ -1,44 +1,24 @@
-#provider "aws" {
-#  region = "var.region_name"
-#  profile = "AWS_741032333307_User"
-#}
-
-provider "aws" {
-  region     = var.region_name
-}
-
-terraform {
-  backend "s3" {
-    bucket = "landg-terraform-state"
-    key    = "eks/test/terraform.tfstate"
-    region = "eu-west-2"
-    encrypt= true
-
-  }
-}
-##
-#resource "aws_s3_bucket" "terraform-state" {
-#  bucket = "landg-terraform-state"
-#  lifecycle {
-#    prevent_destroy = true
-#  }
-#  versioning {
-#    enabled = true
-#  }
-#  server_side_encryption_configuration {
-#    rule {
-#      apply_server_side_encryption_by_default {
-#        sse_algorithm = "AES256"
-#      }
-#    }
-#  }
+#terraform {
+#  backend "s3" {
+#    bucket = "landg-terraform-state"
+#    key    = "eks/test/terraform.tfstate"
+#    region = "eu-west-2"
+#    encrypt= true
 #
+#  }
 #}
+provider "aws" {
+  region = var.region_name
+  profile = var.user_profile
+  #shared_credentials_files = "/c/Users/deepak.haridas.kadam/.aws/credentials"
+}
+
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  config_path = "./.kube/config"
+  config_path = "~/.kube/config"
 }
 
 provider "helm" {
@@ -46,7 +26,8 @@ provider "helm" {
     host                   = data.aws_eks_cluster.eks_cluster.endpoint
     token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-    config_path = "./.kube/config"
-    
+    config_path = "~/.kube/config"
+
   }
 }
+
