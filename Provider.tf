076709e07f636c790ec2a636d0f0ab3a570f9175
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-06-07 19:50:34 +0530

    test2

--- Provider.tf
+++ Provider.tf
@@ -5,6 +5,10 @@
 
 provider "aws" {
   region     = var.region_name
+  access_key = "ASIA2ZCHYB7545PS5JFS"
+  secret_key = "akUNT1fMZdJS+uGlB2UpGYOfG/UiBx+Sgokxl9kV"
+  token      = "IQoJb3JpZ2luX2VjEN7//////////wEaCXVzLWVhc3QtMSJHMEUCIAp5Cx5BpSZsCFCSewW8kvkhnqs/inUE7d9C0RsSqRRzAiEAsaJQQitUzesrMv+U1U6bL+sFrZ58FknCMPCFvfiret0quAII1///////////ARACGgw3NDEwMzIzMzMzMDciDPOa8twKck+m30AYxCqMApIs2CzMxABtvJZHYQ7AXxVoTEcfJC6AJw09MlLsiBh6FU1qv5mfkhwHeGo6ozDyJhAIC4q8+Jm884dLb73wn78/wFVHEEtmOsSw1FZYzEbKM3ant4UeEJ5isOur4P+Lz0IrXXmRp+PyEB0gNH7y7GHE4u011WiqYAIayGUEWQXzzksbzNecNLmhwrhkNASYhojLpuLgO2g+IaQjp6vRJNh1QIjORY4Qy9d9lBqdjO2SzOy3/X+rkAXgLIRrUqBkHjEAB41jjTLu4apK52WkwMWKDAwFsWosIgSpy3Xq0QLseUyVH4/3K2Jqes/pQP0JInnX9BlCs/YE9YWjeHXBFpofXTLwIBBMuwHiiA8wxK/9lAY6nQGMKjJxrq81uBefPRVOQas/EBAYYIlfgeBfE+u33tIqLL5/NWYDyFWPT0KpnLJFmefi0BOTTPO5bAnn09OFcF1LbJmxew3uVhMhC1meppJtXsZyWqOE6mCglH9S0wRb0JvVcAZ6RzVAbmFk+TkHodpvYEbGzSdl39qXwBi0nlgmDN8urV/VZYui5oVLdShvdbCjuxkkIjGM2ZV8ovr2"
+
 }
 terraform {
   backend "s3" {
