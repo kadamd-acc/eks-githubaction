d9c70d4e4019448610e28c62a504d20b37c14a8c
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-06-09 22:13:18 +0530

    deploy3

--- eks-infra/Provider.tf
+++ eks-infra/Provider.tf
@@ -15,30 +15,12 @@ terraform {
 
   }
 }
-##
-#resource "aws_s3_bucket" "terraform-state" {
-#  bucket = "landg-terraform-state"
-#  lifecycle {
-#    prevent_destroy = true
-#  }
-#  versioning {
-#    enabled = true
-#  }
-#  server_side_encryption_configuration {
-#    rule {
-#      apply_server_side_encryption_by_default {
-#        sse_algorithm = "AES256"
-#      }
-#    }
-#  }
-#
-#}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  config_path = "/home/runner/.kube/config"
+  config_path = "/home/runner/.kube/config-custom"
 }
 
 provider "helm" {
@@ -46,7 +28,7 @@ provider "helm" {
     host                   = data.aws_eks_cluster.eks_cluster.endpoint
     token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-    config_path = "/home/runner/.kube/config"
+    config_path = "/home/runner/.kube/config-custom"
 
   }
 }
