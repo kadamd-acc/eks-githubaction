e48218c1570f3699f8c196a1bf4cda8c8a1ff9b6
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-06-09 11:38:02 +0530

    updated the code10

--- eks-infra/main.tf
+++ eks-infra/main.tf
@@ -56,23 +56,23 @@ module "coredns_patching" {
 
 #
 #
-#module "aws_alb_controller" {
-#  source  = "../modules/aws-lb-controller"
-#  k8s_cluster_type = var.cluster_type
-#  k8s_namespace    = "kube-system"
-#  k8s_cluster_name = module.eks.eks_cluster_name
-# # alb_controller_depends_on =  ""
-#  depends_on = [module.eks, module.coredns_patching]
-#}
-##
-#module "eks_kubernetes_addons" {
-#  source         = "../modules/kubernetes-addons"
-#  enable_amazon_eks_vpc_cni    = true
-#  k8s_cluster_type = var.cluster_type
-#  k8s_namespace    = "kube-system"
-#  k8s_cluster_name = module.eks.eks_cluster_name
-#  depends_on = [module.aws_alb_controller]
-#}
+module "aws_alb_controller" {
+  source  = "../modules/aws-lb-controller"
+  k8s_cluster_type = var.cluster_type
+  k8s_namespace    = "kube-system"
+  k8s_cluster_name = module.eks.eks_cluster_name
+ # alb_controller_depends_on =  ""
+  depends_on = [module.eks, module.coredns_patching]
+}
+#
+module "eks_kubernetes_addons" {
+  source         = "../modules/kubernetes-addons"
+  enable_amazon_eks_vpc_cni    = true
+  k8s_cluster_type = var.cluster_type
+  k8s_namespace    = "kube-system"
+  k8s_cluster_name = module.eks.eks_cluster_name
+  depends_on = [module.aws_alb_controller]
+}
 #
 #
 #module "kubernetes_app" {
