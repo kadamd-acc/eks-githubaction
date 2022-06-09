dbbe1833d8c046b7f2068565abb2f0ab63b6270b
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-06-09 23:26:02 +0530

    destroy5

--- eks-infra/Provider.tf
+++ eks-infra/Provider.tf
@@ -20,7 +20,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  config_path = "/home/runner/.kube/config-custom"
+  config_path = "/home/runner/.kube/config"
 }
 
 provider "helm" {
@@ -28,7 +28,7 @@ provider "helm" {
     host                   = data.aws_eks_cluster.eks_cluster.endpoint
     token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-    config_path = "/home/runner/.kube/config-custom"
+    config_path = "/home/runner/.kube/config"
 
   }
 }
