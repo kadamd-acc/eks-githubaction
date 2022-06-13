13c32a3a17f1f5c67748fe405c5c28ed4936524d
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-06-13 17:10:02 +0530

    updating the code for eks config file9

--- eks-infra/main.tf
+++ eks-infra/main.tf
@@ -1,38 +1,38 @@
 
 
 
-#module "vpc" {
-#    source                              = "../modules/vpc"
-#    environment                         =  var.environment
-#    vpc_cidr                            =  var.vpc_cidr
-#    vpc_name                            =  var.vpc_name
-#    cluster_name                        =  var.cluster_name
-#    cluster_group                       =  var.cluster_group
-#    public_subnets_cidr                 =  var.public_subnets_cidr
-#    availability_zones_public           =  var.availability_zones_public
-#    private_subnets_cidr                =  var.private_subnets_cidr
-#    availability_zones_private          =  var.availability_zones_private
-#    cidr_block_nat_gw                   =  var.cidr_block_nat_gw
-#    cidr_block_internet_gw              =  var.cidr_block_internet_gw
-#}
-#
-#
-#
-#module "eks" {
-#    source                              =  "../modules/eks"
-#    cluster_name                        =  var.cluster_name
-#    cluster_version                     =  var.cluster_version
-#    environment                         =  var.environment
-#    #eks_node_group_instance_types       =  var.eks_node_group_instance_types
-#    private_subnets                     =  module.vpc.aws_subnets_private
-#    public_subnets                      =  module.vpc.aws_subnets_public
-#    fargate_app_namespace               =  var.fargate_app_namespace
-#
-#    depends_on = [module.vpc]
-#}
-#
-#
-#
+module "vpc" {
+    source                              = "../modules/vpc"
+    environment                         =  var.environment
+    vpc_cidr                            =  var.vpc_cidr
+    vpc_name                            =  var.vpc_name
+    cluster_name                        =  var.cluster_name
+    cluster_group                       =  var.cluster_group
+    public_subnets_cidr                 =  var.public_subnets_cidr
+    availability_zones_public           =  var.availability_zones_public
+    private_subnets_cidr                =  var.private_subnets_cidr
+    availability_zones_private          =  var.availability_zones_private
+    cidr_block_nat_gw                   =  var.cidr_block_nat_gw
+    cidr_block_internet_gw              =  var.cidr_block_internet_gw
+}
+
+
+
+module "eks" {
+    source                              =  "../modules/eks"
+    cluster_name                        =  var.cluster_name
+    cluster_version                     =  var.cluster_version
+    environment                         =  var.environment
+    #eks_node_group_instance_types       =  var.eks_node_group_instance_types
+    private_subnets                     =  module.vpc.aws_subnets_private
+    public_subnets                      =  module.vpc.aws_subnets_public
+    fargate_app_namespace               =  var.fargate_app_namespace
+
+    depends_on = [module.vpc]
+}
+
+
+
 #module "coredns_patching" {
 #  source  = "../modules/coredns-patch"
 #
